15963718a8549d8135d11113f8b1286214ab300e
Austin <austinphilippaul@gmail.com>
2022-08-03 14:08:05 +0530

    Adding Project

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -7,13 +7,16 @@ const TodoItem=(props)=>{
     const {key,item,isComplete}=props;
     const [complete,setComplete]=useState(isComplete)//might have to use recoil or something to convey the change to local storage or to parent or something
 
-    const onChange=()=>{
-        
+    const onChange=(event)=>{
+       setComplete(!complete);
+    //    if(complete)//executed before value of complete gets updated
+    //     document.getElementById("task").className="line-through"
+    //     event.target.className="rounded-full w-8 h-8 bg-white border-2"
     }
     return(
         <div className="bg-white max-w-sm text-left translate-x-6">
-            <button  type="button"  onChange={onChange} className="rounded-full w-8 h-8 bg-white border-2"/>
-            <input type="text" value={item} className=" h-8 -translate-y-2 "/>
+            <input type="checkbox" checked={complete} onChange={onChange} className="rounded-full w-8 h-8 bg-white border-2 shadow-sm"/>{/* should I add div and change it to circle*/ }
+            <input id="task" type="text" value={item} className=" h-8 shadow-md border-white "/>
         </div>
     )
 }
